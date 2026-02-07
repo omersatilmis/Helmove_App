b4d3f7122c5e91f546c1f4642405a9730d06d7f8
Ömer Satılmış <119844379+omersatilmis@users.noreply.github.com>
2026-02-07 22:48:44 +0300

    feat: implement group settings UI with fields for name, participants, privacy, destination, and riding style.

--- lib/features/communication/presentation/pages/group_settings.dart
+++ lib/features/communication/presentation/pages/group_settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../core/theme/text_styles.dart';
 import '../../../../core/widgets/app_input_field.dart';
