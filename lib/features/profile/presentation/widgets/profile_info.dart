9422cacb7fb406cfea9756eb3e8b94a7e6282f96
Ömer Satılmış <119844379+omersatilmis@users.noreply.github.com>
2026-03-12 03:42:04 +0300

    feat(profile): update text styles for improved readability in profile info

--- lib/features/profile/presentation/widgets/profile_info.dart
+++ lib/features/profile/presentation/widgets/profile_info.dart
@@ -139,14 +139,14 @@ class _AddContentItem extends StatelessWidget {
                 children: [
                   Text(
                     title,
-                    style: AppTextStyles.bodyMedium.copyWith(
+                    style: AppTextStyles.h4.copyWith(
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
                     subtitle,
-                    style: AppTextStyles.bodySmall.copyWith(
+                    style: AppTextStyles.caption.copyWith(
                       color: theme.colorScheme.onSurface.withValues(alpha: 0.6),
                     ),
                   ),
